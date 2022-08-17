b93d4c50ff3efd619fc0be8d653998902c165164
Toni Jarjour <39845673+tonijarjour@users.noreply.github.com>
2022-08-17 13:19:17 -0400

    init

---
+++ patch/include.c
@@ -0,0 +1,21 @@
+/* Bar functionality */
+#include "bar_indicators.c"
+#include "bar_tagicons.c"
+#include "bar.c"
+
+#include "bar_ltsymbol.c"
+#include "bar_status.c"
+#include "bar_status2d.c"
+#include "bar_tags.c"
+#include "bar_wintitle.c"
+#include "bar_awesomebar.c"
+#include "bar_wintitleactions.c"
+
+/* Other patches */
+#include "attachx.c"
+#include "scratchpad.c"
+/* Layouts */
+#include "layout_facts.c"
+#include "layout_monocle.c"
+#include "layout_tile.c"
+
