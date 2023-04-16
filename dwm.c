be73896def78319c1fd400e88f6514c83e2c54c9
Toni Jarjour <39845673+tonijarjour@users.noreply.github.com>
2023-04-16 19:52:25 -0400

    update

--- dwm.c
+++ dwm.c
@@ -2005,6 +2005,8 @@ showhide(Client *c)
 void
 spawn(const Arg *arg)
 {
+	struct sigaction sa;
+
 	if (arg->v == dmenucmd)
 		dmenumon[0] = '0' + selmon->num;
 
@@ -2014,6 +2016,12 @@ spawn(const Arg *arg)
 			close(ConnectionNumber(dpy));
 
 		setsid();
+
+		sigemptyset(&sa.sa_mask);
+		sa.sa_flags = 0;
+		sa.sa_handler = SIG_DFL;
+		sigaction(SIGCHLD, &sa, NULL);
+
 		execvp(((char **)arg->v)[0], (char **)arg->v);
 		die("dwm: execvp '%s' failed:", ((char **)arg->v)[0]);
 	}
